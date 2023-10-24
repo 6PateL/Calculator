c4717aa6098dbdf2d19107ed73bd3d9486376b41
6PaTeL <jkdjkd789@gmail.com>
2023-10-24 12:09:00 +0300

    Add project files.

---
+++ Calculator/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region buttons
+        private void AddOne(object sender, EventArgs e)
+        {
+            textBox1.Text += "1"; 
+        }
+
+        private void AddTwo(object sender, EventArgs e)
+        {
+            textBox1.Text += "2"; 
+        }
+
+        private void AddThree(object sender, EventArgs e)
+        {
+            textBox1.Text += "3"; 
+        }
+
+        private void AddFour(object sender, EventArgs e)
+        {
+            textBox1.Text += "4"; 
+        }
+
+        private void AddFive(object sender, EventArgs e)
+        {
+            textBox1.Text += "5";
+        }
+
+        private void AddSix(object sender, EventArgs e)
+        {
+            textBox1.Text += "6"; 
+        }
+
+        private void AddSeven(object sender, EventArgs e)
+        {
+            textBox1.Text += "7"; 
+        }
+
+        private void AddEight(object sender, EventArgs e)
+        {
+            textBox1.Text += "8"; 
+        }
+
+        private void AddNine(object sender, EventArgs e)
+        {
+            textBox1.Text += "9"; 
+        }
+
+        private void AddZero(object sender, EventArgs e)
+        {
+            textBox1.Text += "0"; 
+        }
+
+        private void AddLeftParenthesis(object sender, EventArgs e)
+        {
+            textBox1.Text += "("; 
+        }
+
+        private void AddRightParenthesis(object sender, EventArgs e)
+        {
+            textBox1.Text += ")"; 
+        }
+        #region Plus,Minus,Multiply,Substract
+        private void AddPlus(object sender, EventArgs e)
+        {
+            textBox1.Text += "+";
+
+            if (CheckNextSymbol(textBox1.Text) == true)
+            {
+                textBox1.Text = RemoveNextSymbol(textBox1.Text);
+            }
+        }
+
+        private void AddMinus(object sender, EventArgs e)
+        {
+            textBox1.Text += "-";
+
+            if (CheckNextSymbol(textBox1.Text) == true)
+            {
+                textBox1.Text = RemoveNextSymbol(textBox1.Text);    
+            }
+        }
+
+        private void AddMultiply(object sender, EventArgs e)
+        {
+            textBox1.Text += "*";
+
+            if (CheckNextSymbol(textBox1.Text) == true)
+            {
+                textBox1.Text = RemoveNextSymbol(textBox1.Text);
+            }
+        }
+
+        private void AddSubstract(object sender, EventArgs e)
+        {
+            textBox1.Text += "/";
+
+            if (CheckNextSymbol(textBox1.Text) == true)
+            {
+                textBox1.Text = RemoveNextSymbol(textBox1.Text);
+            }
+        }
+        #endregion
+        #endregion
+
+        private void Clear(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty; 
+        }
+
+        private void Eaqual(object sender, EventArgs e)
+        {
+            StringAnalyze analyze = new StringAnalyze(textBox1.Text);
+            textBox1.Text = analyze.GetResult(); 
+        }
+
+        private bool CheckNextSymbol(string value)
+        {
+            for(int i = 0; i < value.Length - 1; i++)
+            {
+                if (value[i].CompareTo(value[i + 1]) == 0)
+                {
+                    return true; 
+                }
+            }
+            return false; 
+        }
+
+        private static string RemoveNextSymbol(string value)
+        {
+            var charValue = value.ToString();
+            List<char> symbols = new List<char>();
+            symbols.AddRange(charValue);
+
+            for (int i = 0; i < symbols.Count - 1; i++)
+            {
+                if (symbols[i] == symbols[i + 1])
+                {
+                    symbols.Remove(symbols[i + 1]);
+                }
+            }
+
+            return new string(symbols.ToArray());
+        }
+    }
+}
---
+++ Calculator/StringAnalyze.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class StringAnalyze
+    {
+        private string _expression; 
+
+        public StringAnalyze(string expression) 
+        { 
+            _expression = expression;   
+        }
+
+        public StringAnalyze()
+        {
+            
+        }
+
+        public void SetExpression(string expression)
+        {
+            _expression = expression; 
+        }
+
+        public string GetResult()
+        {
+            string postfix = ConvertToPostfix(_expression);
+            int result = EvaluatePostfix(postfix);
+            return result.ToString(); 
+        }
+
+        private string ConvertToPostfix(string expression)
+        {
+            var output = new List<char>();
+            var stack = new Stack<char>();
+
+            foreach (var c in expression)
+            {
+                if (char.IsDigit(c))
+                {
+                    output.Add(c);
+                }
+                else if (c == '(')
+                {
+                    stack.Push(c);
+                }
+                else if (c == ')')
+                {
+                    while (stack.Count > 0 && stack.Peek() != '(')
+                    {
+                        output.Add(stack.Pop());
+                    }
+
+                    if (stack.Count > 0 && stack.Peek() == '(')
+                    {
+                        stack.Pop();
+                    }
+                }
+                else
+                {
+                    while (stack.Count > 0 && GetPrecedence(c) <= GetPrecedence(stack.Peek()))
+                    {
+                        output.Add(stack.Pop());
+                    }
+
+                    stack.Push(c);
+                }
+            }
+
+            while (stack.Count > 0)
+            {
+                output.Add(stack.Pop());
+            }
+
+            return new string(output.ToArray());
+        }
+
+        private int EvaluatePostfix(string postfix)
+        {
+            var stack = new Stack<int>();
+
+            foreach (var c in postfix)
+            {
+                if (char.IsDigit(c))
+                {
+                    stack.Push(c - '0');
+                }
+                else
+                {
+                    int operand2 = stack.Pop();
+                    int operand1 = stack.Pop();
+
+                    switch (c)
+                    {
+                        case '+':
+                            stack.Push(operand1 + operand2);
+                            break;
+                        case '-':
+                            stack.Push(operand1 - operand2);
+                            break;
+                        case '*':
+                            stack.Push(operand1 * operand2);
+                            break;
+                        case '/':
+                            stack.Push(operand1 / operand2);
+                            break;
+                        default:
+                            throw new ArgumentException($"Invalid operator: {c}");
+                    }
+                }
+            }
+
+            return stack.Pop();
+        }
+
+        private int GetPrecedence(char op)
+        {
+            switch (op)
+            {
+                case '+':
+                case '-':
+                    return 1;
+                case '*':
+                case '/':
+                    return 2;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
